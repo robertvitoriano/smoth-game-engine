d6e63a98e22369aed9818bc8b7ceeaed4b2ad2e8
Robert Vitoriano <robertvitorianodev@gmail.com>
2026-05-01 10:46:08 -0300

    create window

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <iostream>
 #include <GLFW/glfw3.h>
 
 int main()
@@ -6,6 +6,30 @@ int main()
 #if defined(__linux__)
   glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
 #endif
+
+  if (!glfwInit())
+  {
+    return -1;
+  }
+
+  GLFWwindow *window = glfwCreateWindow(1280, 780, "SmothGameEngine", nullptr, nullptr);
+
+  if (window == nullptr)
+  {
+    std::cout << "Error creating Window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwSetWindowPos(window, 0, 150);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
   printf("Hello, world!\n");
   return 0;
 }
