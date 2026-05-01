070a34389c88e69082aead27ba78b290bbbc55da
Robert Vitoriano <robertvitorianodev@gmail.com>
2026-05-01 10:45:50 -0300

    create window

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
+#if defined(__linux__)
+  glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+#endif
   printf("Hello, world!\n");
   return 0;
 }
